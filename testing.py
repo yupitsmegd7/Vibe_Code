5c73eff326789732d01e2c885d7401dea4becf37
Gourav <gouravduttagd7@gmail.com>
2026-05-22 09:56:58 +0530

    more stable version

--- testing.py
+++ testing.py
@@ -1,1 +1,7 @@
 print("Hello world")
+print("S what do you think, the world is pretty doomed isn't it")
+i=input("Enter your opinion")
+if i=="yes":
+    print("Nihialist bruh")
+else:
+    print("good boy")
