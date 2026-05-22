7e10679efffddc87867828bfdf4a2b0b7549a0b3
Gourav <gouravduttagd7@gmail.com>
2026-05-22 11:09:15 +0530

    testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,5 @@
-print("Hello world")
-print("S what do you think, the world is pretty doomed isn't it")
-i=input("Enter your opinion")
-if i=="yes":
-    print("Nihialist bruh")
-else:
-    print("good boy")
+
+print("So what do you think, the world is pretty doomed isn't it")
+print("Do you think"
+      "Crying for others"
+      "Makes you a sick person")
